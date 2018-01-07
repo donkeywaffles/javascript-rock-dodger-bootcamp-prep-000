456e0c168a9c4b5a162d3200774f2b4580ee833a
Jonathan Valdesuso <jbvaldesuso@gmail.com>
2018-01-07 04:31:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -167,7 +167,6 @@ function moveDodgerRight() {
   var left = positionToInteger(dodger.style.left);
   if (left < 360) {
     dodger.style.left = `${left + 4}px`;
-    window.requestAnimationFrame(moveDodgerRight);
   }
 }
 
